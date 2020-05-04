ff7e3fa776a41f31dceae258c4af17faa338ea59
gweiler <gweiler@yext.com>
2020-05-04 13:29:13 -0400

    adding something to try branch

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,7 +5,7 @@ import Layout from "../components/layout"
 export default ( {data} ) => (
   <Layout>
     <h1>About {data.site.siteMetadata.title} </h1>
-    <h2> Gideon is on a new branch</h2>
+    <h2> Gideon is on the try branch!</h2>
     <p>
       We're the only site running on your computer dedicated to showing the best
       photos and videos of pandas eating lots of food.
